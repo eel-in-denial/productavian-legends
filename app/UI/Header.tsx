0ea39f23b2c93e2daaf9a84085794ff24bb3522d
Daniel Lee <danielwlee515@gmail.com>
2025-06-05 11:54:24 +1000

    deployment fixes

--- app/UI/Header.tsx
+++ app/UI/Header.tsx
@@ -1,6 +1,3 @@
-import Link from 'next/link'
-import Image from "next/image";
-
 export default function Header() {
     return (
         <div className='h-1/8 border-b-1'>
--- app/habits/page.tsx
+++ app/habits/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Habits() {
   return (
     <div className="bg-blue-50">
