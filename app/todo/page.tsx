a301dc919fbabd3c32eafbeb3b0f041910b7af7d
Daniel Lee <danielwlee515@gmail.com>
2025-06-05 12:20:55 +1000

    found the thing breaking stuff
    and commented it out

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -4,24 +4,24 @@ export default function ToDo() {
     <div className="flex-col items-center w-full h-full p-5 px-10 text-center">
       <h1 className="text-2xl pb-2">To Do</h1>
       <div>
-        <TodoItem text='running'/>
+        {/* <TodoItem text='running'/> */}
       </div>
-      <button onClick={AddTodo} className="bg-gray-300 font-bold text-xl w-10 rounded-b-full hover:bg-gray-500 active:bg-black">
+      {/* <button onClick={AddTodo} className="bg-gray-300 font-bold text-xl w-10 rounded-b-full hover:bg-gray-500 active:bg-black">
         +
-      </button>
+      </button> */}
     </div>
   );
 }
 
-export const TodoItem = ({text=''}) => {
-  return(
-    <div className="flex gap-5 border w-full p-1 px-3">
-      <input type="text" className="w-full"></input>
-      <input type="checkbox" className=""/>
-    </div>
-  )
-}
+// const TodoItem = ({text} : {text: string}) => {
+//   return(
+//     <div className="flex gap-5 border w-full p-1 px-3">
+//       <input type="text" className="w-full">{text}</input>
+//       <input type="checkbox" className=""/>
+//     </div>
+//   )
+// }
 
-function AddTodo() {
+// function AddTodo() {
   
-}
+// }
